c93c53798ac83dc78d7adf2227a50638485cfe3b
mtuushke-ne <mtuushke@gmail.com>
2026-09-20 22:27:18 +0800

    Initial commit

---
+++ functions.c
@@ -0,0 +1,46 @@
+#include "myinclude.h"
+
+void read(int A[], int n)
+{
+       
+}
+
+void print(int A[], int n)
+{
+       
+}
+
+int max(int A[], int n)
+{
+        
+}
+
+int min(int A[], int n)
+{
+        
+}
+
+void copy(int A[], int n, int B[])
+{
+        
+}
+
+int find(int A[], int n, int x)
+{
+        
+}
+
+int make_set(int A[], int n, int B[])
+{
+       
+}
+
+int union_set(int A[], int n, int B[], int m)
+{
+        
+}
+
+int intersection_set(int A[], int n, int B[], int m, int C[])
+{
+        
+}
